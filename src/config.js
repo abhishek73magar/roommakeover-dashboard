ec49337f289e8c75f4ac04d29851fdad9c9c5050
Abhishek Magar <abhishek.magar@rts.com.np>
2024-03-05 08:54:26 +0545

    confilt change

--- src/config.js
+++ src/config.js
@@ -5,4 +5,4 @@ const dev = import.meta.env.VITE_DEV
 
 export const BASE_URL = import.meta.env.MODE === 'production' ? prod : dev
 export const IMAGE_URL = `${BASE_URL}/api/image`
-// export const IMAGE_URL = `https://betaadmin.roommakeover.com.np/api/image`
+//export const IMAGE_URL = `https://roommakeover.com.np/api/image`

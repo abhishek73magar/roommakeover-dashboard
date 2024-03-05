f2956191da1db8e222b4b0a11c615416769c11c5
abhishek <Coder73-avi>
2024-03-05 08:55:55 +0545

    confilt merge

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const prod = import.meta.env.VITE_PROD
-const dev = import.meta.env.VITE_DEV
-// const dev = 'http://localhost:4001'
+// const dev = import.meta.env.VITE_DEV
+const dev = 'http://localhost:4001'
 
 
 export const BASE_URL = import.meta.env.MODE === 'production' ? prod : dev
-export const IMAGE_URL = `${BASE_URL}/api/image`
-//export const IMAGE_URL = `https://roommakeover.com.np/api/image`
+// export const IMAGE_URL = `${BASE_URL}/api/image`
+export const IMAGE_URL = `https://betaadmin.roommakeover.com.np/api/image`
